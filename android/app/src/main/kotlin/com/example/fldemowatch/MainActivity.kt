97987150d7c0bd858b809ebc9330f8e14647bf23
Phil Magnuson <phil@dharmadr.com>
2023-08-29 11:05:11 -0600

    starting point

---
+++ android/app/src/main/kotlin/com/example/fldemowatch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fldemowatch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Dharma Dr Watch App/Dharma_DrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dharma_DrApp.swift
+//  Dharma Dr Watch App
+//
+//  Created by Phil Magnuson on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Dharma_Dr_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
